72d3ddccecfe1c13d24fe00cc2b75c5eb9143700
Viet Hoang <hoangproo2624@gmail.com>
2024-09-22 09:52:15 +0700

    simple react nginx

---
+++ fe-nginx/simple-react-nginx/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to React App deployed with Nginx & Docker!</h1>
+      <p>This is a simple React app served by Nginx inside a Docker container.</p>
+    </div>
+  );
+}
+
+export default App;
